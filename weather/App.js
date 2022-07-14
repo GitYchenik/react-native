d5a0926c687d9dc19b3866a3535999b6570d9a79
Albert <gooferstotjyu@yandex.ru>
2022-07-14 18:26:17 +0600

    update 14/07/2022

--- weather/App.js
+++ weather/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Albrto</Text>
+      <Text>Hello Alberto</Text>
       <StatusBar style="auto" />
     </View>
   );
