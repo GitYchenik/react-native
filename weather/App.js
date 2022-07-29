ede3c5e65d0341a80d063ab4555b8cce2c38e315
Albert <gooferstotjyu@yandex.ru>
2022-07-29 11:27:49 +0600

    update 29/07/2022

--- weather/App.js
+++ weather/App.js
@@ -1,9 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Alert} from 'react-native';
+import * as Location from 'expo-location';
 import Loading from './Loading';
 
-export default function App() {
-  return (
-    <Loading />
-  );
+
+export default class extends React.Component {
+
+  getLocation = async () => {
+    try {
+      //если истина то выполни
+      await Location.requestBackgroundPermissionsAsync();
+      //const response = await Location.requestPermissionsAsync();-старый формат
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
+      //Получения из свойств coords кардинаты по гориз и вертикали т.е. lattude longitude
+    } catch (error) {
+      Alert.alert('Не могу определить местоположение', "Очень грустно :(");
+    }
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render () {
+    return (
+      <Loading />
+    );
+  }
 }
