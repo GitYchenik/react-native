4d3648bb3472d5a34350ae7f5a27b98fd0b2371b
Albert <gooferstotjyu@yandex.ru>
2022-08-02 22:08:00 +0600

    update 02/08/2022

--- weather/App.js
+++ weather/App.js
@@ -1,33 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Alert } from 'react-native';
+import { Alert} from 'react-native';
 import * as Location from 'expo-location';
 import Loading from './Loading';
-import axios from'axios';
+import Weather from './weather';
+import axios from 'axios';
+import React from 'react';
 
 const API_KEY = 'e01567664ab7f7e555a8a40f4854cb8b';   //ы созданую переменную укладываем ключ доступа API полученные на сайте
+
 export default class extends React.Component {
 
   state = {
     isLoading: true
   }
 
-  getWeather = async(latitude, longitude) => {
-    const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-    console.log(data);
-  }
+getWeather = async (latitude, longitude) => {
+  const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+  this.setState({isLoading: false, temp: data.main.temp});
+  console.log(data);
+}
+
 
   getLocation = async () => {
     try {//если истина то выполни
-      await Location.requestBackgroundPermissionsAsync();   //const response = await Location.requestPermissionsAsync();-старый формат
-      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();     //Получения из свойств coords кардинаты по гориз и вертикали т.е. lattude longitude
-      console.log({ coords: { latitude, longitude } })  //для проверки показывает координаты широту и долготу
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });//убрать окно загрузки
-
     } catch (error) {
-      Alert.alert('Не могу определить местоположение', "Очень грустно :(");//сообщение в случае если не получилось загрузить информацию
+      Alert.alert('Не могу определить местоположение', "Очень грустно :(");
     }
   }
 
@@ -35,9 +34,11 @@ export default class extends React.Component {
     this.getLocation();
   }
 
-  render() {
-    return (
-      <Loading />   //прорисовывает экран из loading.js
-    );
-  }
+
+render () {
+  const {isLoading, temp} = this.state;
+  return (
+    isLoading ? <Loading /> : <Weather  temp={Math.round(temp)}/>
+  );
+}
 }
